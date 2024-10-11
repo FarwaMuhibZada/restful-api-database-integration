a3681cb5d7f26b16cc4fa7de0fbf26b2706b2814
farwaMuhibzada <farwafarid2017@gmail.com>
2024-10-11 16:25:27 +0430

    added restful apis for fetching a user by id .

--- server.js
+++ server.js
@@ -19,6 +19,17 @@ app.get('/users', async(req, res) => {
   }
 })
 
+// get users by id 
+app.get('/users/:id', async(req, res) =>{
+  try {
+      const {id} = req.params;
+      const user = await User.findById(id);
+      res.status(200).json(user);
+  } catch (error) {
+      res.status(500).json({message: error.message})
+  }
+})
+
 app.post('/users', async(req, res) => {
   try {
       const user = await User.create(req.body)
