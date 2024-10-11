7c1be035c4048680715aab789fe55943e26d439c
farwaMuhibzada <farwafarid2017@gmail.com>
2024-10-11 15:21:00 +0430

    create database connectivity with mangodb atlas .

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 //Routes
 app.get('/', (req, res) => {
-    res.send('Hello NODE API');
-})
-app.listen(3000, ()=> {
-    console.log(`Node API app is running on port 3000`);
+  res.send('Hello NODE API');
 });
+
+const dbUrl = 'mongodb+srv://admin:pwd1234@my-cluster.maxsh.mongodb.net/?retryWrites=true&w=majority&appName=my-cluster';
+mongoose.connect(dbUrl)
+  .then(() => {
+    console.log('connected to MongoDB');
+    app.listen(3000, () => {
+      console.log(`Node API app is running on port 3000`);
+    });
+  }).catch((error) => {
+    console.log(error)
+  })
