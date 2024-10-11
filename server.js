888a02bef2af68e75743ede1d90bd740930fa2be
farwaMuhibzada <farwafarid2017@gmail.com>
2024-10-11 16:35:25 +0430

    added api for update the specific user .

--- server.js
+++ server.js
@@ -10,34 +10,50 @@ app.get('/', (req, res) => {
 });
 
 // fetch data from Node-API database using this route
-app.get('/users', async(req, res) => {
+app.get('/users', async (req, res) => {
   try {
-      const users = await User.find({});
-      res.status(200).json(users);
+    const users = await User.find({});
+    res.status(200).json(users);
   } catch (error) {
-      res.status(500).json({message: error.message})
+    res.status(500).json({ message: error.message })
   }
 })
 
 // get users by id 
-app.get('/users/:id', async(req, res) =>{
+app.get('/users/:id', async (req, res) => {
   try {
-      const {id} = req.params;
-      const user = await User.findById(id);
-      res.status(200).json(user);
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
   } catch (error) {
-      res.status(500).json({message: error.message})
+    res.status(500).json({ message: error.message })
   }
 })
 
-app.post('/users', async(req, res) => {
+// update or edit the user
+app.put('/users/:id', async (req, res) => {
   try {
-      const user = await User.create(req.body)
-      res.status(200).json(user);
-      
+    const { id } = req.params;
+    const user = await User.findByIdAndUpdate(id, req.body);
+    // we cannot find any product in database
+    if (!user) {
+      return res.status(404).json({ message: `cannot find any product with ID ${id}` })
+    }
+    const updatedUser = await User.findById(id);
+    res.status(200).json(updatedUser);
+
   } catch (error) {
-      console.log(error.message);
-      res.status(500).json({message: error.message})
+    res.status(500).json({ message: error.message })
+  }
+})
+app.post('/users', async (req, res) => {
+  try {
+    const user = await User.create(req.body)
+    res.status(200).json(user);
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message })
   }
 });
 
